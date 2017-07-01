b4f0163cd15aa668c26ca8048613373e41a0ec35
Sheng Di <SDi@walmartlabs.com>
2017-07-01 11:10:43 -0700

    wip

--- packages/oss-component/demo/demo.jsx
+++ packages/oss-component/demo/demo.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import Demo from "electrode-demo-index";
+import { addLocaleData, IntlProvider } from 'react-intl';
+
 import * as libraryScope from "../src/index";
-import "./demo.css";
 
 const locale = "en";
 const messages = require(`../src/lang/${locale}.json`);
+const localeData = require(`react-intl/locale-data/${locale}`);
+
+addLocaleData(localeData);
 
 const components = [
   {
+    title: "Component Title",
     examples: [
       {
         type: "playground",
-        code: require("raw-loader!./examples/oss-component.example")
+        code: require("raw-loader!./examples/oss-component.example"),
+        noRender: true
       } // any additional examples here
     ]
   } // any additional components here
 ];
+const localScope = {IntlProvider, messages, locale};
 
 const demo = () => <Demo libraryScope={libraryScope} components={components} />;
 
